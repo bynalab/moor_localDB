7e5a21440d6a757bb2a82db58e3815a83035e6da
Abubakar Abdusalam <bynalabs@gmail.com>
2020-08-26 06:41:59 +0100

    added 'update' part of the CRUD

--- lib/Intents/view_users.dart
+++ lib/Intents/view_users.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:moor_database/database/moor_database.dart';
+import 'package:moor_database/forms/registration.dart';
 import 'package:provider/provider.dart';
 
 class ViewUser extends StatefulWidget {
@@ -49,7 +50,46 @@ class _ViewUser extends State<ViewUser> {
           caption: 'Edit',
           color: Colors.black45,
           icon: Icons.edit,
-          onTap: () {},
+          onTap: () {
+            showCupertinoDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return SizedBox(
+                  height: 800,
+                  child: CupertinoPopupSurface(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Editing ' + user.name,
+                          style: TextStyle(
+                              // color: Colors.deepPurple,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w900),
+                        ),
+                        Registration(
+                          id: user.id,
+                          title: user.title,
+                          name: user.name,
+                          email: user.email,
+                          phone: user.phone,
+                        ),
+                        CupertinoButton(
+                          child: Text("Cancel"),
+                          color: Colors.red,
+                          onPressed: () {
+                            Navigator.pop(context);
+                            // resetForm();
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
         ),
         IconSlideAction(
             caption: 'Delete',
---
+++ lib/forms/registration.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:moor_database/database/moor_database.dart';
+import 'package:provider/provider.dart';
+
+class Registration extends StatefulWidget {
+  final String title;
+  final String name;
+  final String email;
+  final String phone;
+  final int id;
+  // final int chapter, verse;
+  Registration(
+      {Key key, this.title, this.name, this.email, this.phone, this.id})
+      : super(key: key);
+  // Registration(this.name);
+  @override
+  _RegistrationState createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 40),
+                child: Text(
+                  "Title   ",
+                ),
+              ),
+              Expanded(
+                child: CupertinoTextField(
+                  controller: titleController ?? titleController
+                    ..text = widget.title,
+                  prefix: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.title,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  placeholder: 'Mr.',
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 30),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 40),
+                child: Text(
+                  "Name",
+                ),
+              ),
+              Expanded(
+                child: CupertinoTextField(
+                  controller: nameController ?? nameController
+                    ..text = widget.name,
+                  prefix: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      CupertinoIcons.person_solid,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  placeholder: 'Abubakar Abdusalam',
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 45),
+                child: Text("Email"),
+              ),
+              Expanded(
+                child: CupertinoTextField(
+                  controller: emailController ?? emailController
+                    ..text = widget.email,
+                  prefix: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      CupertinoIcons.mail_solid,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  placeholder: 'bynalabs@gmail.com',
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 40),
+                child: Text(
+                  "Phone",
+                ),
+              ),
+              Expanded(
+                child: CupertinoTextField(
+                  controller: phoneController ?? phoneController
+                    ..text = widget.phone,
+                  prefix: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      CupertinoIcons.phone_solid,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  placeholder: '+234701298668',
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 30),
+          CupertinoButton(
+            child: Text("Submit"),
+            color: Colors.deepPurple,
+            onPressed: () {
+              final db = Provider.of<UserDB>(context, listen: false);
+              final user = User(
+                  id: null,
+                  title: titleController.text,
+                  name: nameController.text,
+                  email: emailController.text,
+                  phone: phoneController.text,
+                  status: true);
+
+              db.updateUser(
+                user.copyWith(
+                    id: widget.id,
+                    title: titleController.text,
+                    name: nameController.text,
+                    email: emailController.text,
+                    phone: phoneController.text),
+              );
+              Navigator.pop(context);
+              // resetForm();
+            },
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  void resetForm() {
+    setState(() {
+      titleController.clear();
+      nameController.clear();
+      emailController.clear();
+      phoneController.clear();
+    });
+  }
+}
