33ce3554b45a84ded0774bd1da2eecf599d4364f
Abubakar Abdusalam <bynalabs@gmail.com>
2021-01-11 19:53:03 +0100

    clean code

--- lib/Intents/tabview.dart
+++ lib/Intents/tabview.dart
@@ -5,9 +5,8 @@ import 'package:moor_database/Intents/edit_user.dart';
 import 'package:moor_database/Intents/view_users.dart';
 
 class TabView extends StatefulWidget {
-  // final String title;
 
-  // AddUser({Key key, this.title}) : super(key: key);
+  AddUser({Key key}) : super(key: key);
   @override
   State<StatefulWidget> createState() => _TabView();
 }
@@ -16,14 +15,11 @@ class _TabView extends State<TabView> {
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
-      // backgroundColor: Colors.deepPurple,
       tabBar: CupertinoTabBar(backgroundColor: CupertinoColors.white, items: [
         BottomNavigationBarItem(
             icon: Icon(CupertinoIcons.add), title: Text("Add User")),
         BottomNavigationBarItem(
             icon: Icon(CupertinoIcons.add), title: Text("View Users")),
-        // BottomNavigationBarItem(
-        //     icon: Icon(CupertinoIcons.eye), title: Text("Edit Users"))
       ]),
       tabBuilder: (BuildContext context, int index) {
         switch (index) {
@@ -40,23 +36,6 @@ class _TabView extends State<TabView> {
             return Text("Users");
             break;
         }
-        // return CupertinoTabView(
-        //   builder: (context) {
-        //     return CupertinoPageScaffold(
-        //         // backgroundColor: CupertinoColors.white,
-        //         navigationBar: CupertinoNavigationBar(
-        //           middle: (index == 0) ? Text('Add User') : Text('View User'),
-        //         ),
-        //         child: Center(
-        //           child: Text(
-        //             'Bynalab $index!',
-        //             style: CupertinoTheme.of(context)
-        //                 .textTheme
-        //                 .navLargeTitleTextStyle,
-        //           ),
-        //         ));
-        //   },
-        // );
       },
     );
   }
